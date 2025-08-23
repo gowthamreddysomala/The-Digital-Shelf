79edd0c7b248d1dc494ea28a2946c4cc76cda302
gowthamreddy <gowthamreddysomala@gmail.com>
2025-08-23 19:06:16 +0530

    v0.1 and Created Frontend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,12 +1,11 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',   // keep relative paths
   server: {
     port: 3000,
     open: true
   }
 })
-
